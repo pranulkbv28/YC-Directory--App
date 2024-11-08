c91e958d17da4d8d8fc1947d60b9c30ac6db33dc
pranulkbv28 <28bvpranaav@gmail.com>
2024-11-08 17:24:49 +0530

    created schema for author and startup

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { author } from "@/sanity/schemaTypes/author";
+import { startup } from "@/sanity/schemaTypes/startup";
+import { type SchemaTypeDefinition } from "sanity";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [author, startup],
+};
