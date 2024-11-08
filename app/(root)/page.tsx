97a01dbfbb7539c345fe9eacfa5ef7a43fb3a036
pranulkbv28 <28bvpranaav@gmail.com>
2024-11-08 15:14:35 +0530

    added Next Form and utilised shad/cn buttons

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,14 @@
 // import Image from "next/image";
 
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query || "";
+
   return (
     <>
       <section className="pink_container">
@@ -8,8 +16,9 @@ export default function Home() {
           Pitch Your Startup <br /> Connect With Entrepreneurs
         </h1>
         <p className="sub-heading !max-w-3xl">
-          Submit Ideas, Vote on Pitche, and Get Noticed in virtual Competitions
+          Submit Ideas, Vote on Pitches, and Get Noticed in virtual Competitions
         </p>
+        <SearchForm query={query} />
       </section>
     </>
   );
