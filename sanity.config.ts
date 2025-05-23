3b36eafa8635a3aec5f98aace928f6c8bda5d850
pranulkbv28 <28bvpranaav@gmail.com>
2025-05-23 23:34:50 +0530

    add startup page component and update Sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -8,14 +8,19 @@ import { visionTool } from "@sanity/vision";
 import { defineConfig } from "sanity";
 import { apiVersion, dataset, projectId } from "./sanity/env";
 import { schema } from "./sanity/schemaTypes";
-import { structure } from "./sanity/structure";
+// import { structure } from "./sanity/structure";
 import { markdownSchema } from "sanity-plugin-markdown";
+import { structure, structureTool } from "sanity/structure";
 
 export default defineConfig({
   basePath: "/studio",
   projectId,
   dataset,
   schema,
-  plugins: [visionTool({ defaultApiVersion: apiVersion }), markdownSchema()],
-  structure, // Use the structure function directly
+  plugins: [
+    visionTool({ defaultApiVersion: apiVersion }),
+    markdownSchema(),
+    structureTool({ structure }),
+  ],
+  // structure, // Use the structure function directly
 });
